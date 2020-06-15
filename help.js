178292b20ee32d6960cd6d187f96173949552ec9
Vishnu Raj <vishnubhadri@hotmail.com>
2020-06-15 23:26:11 +0530

    Added help.js. Split help page as two div. render onclick from left div.

---
+++ help.js
@@ -0,0 +1,35 @@
+window['help_menu'] = [
+    {
+        "title": "test",
+        "content": [
+            {
+                tag: "h1",
+                text: "test",
+            }
+        ]
+    }, {
+        "title": "test1",
+        "content": [
+            {
+                tag: "h1",
+                text: "test1",
+            }
+        ]
+    }, {
+        "title": "test2",
+        "content": [
+            {
+                tag: "h1",
+                text: "test2",
+            }
+        ]
+    }, {
+        "title": "test3",
+        "content": [
+            {
+                tag: "h1",
+                text: "test3",
+            }
+        ]
+    }
+]
